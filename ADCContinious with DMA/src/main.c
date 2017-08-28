69c75e3f30d5765f244652c5ff4d131cc11c007d
Hristiyan Andreev <hrvalandreev@gmail.com>
2017-08-28 19:20:40 +0300

    1024 ADC Samples with proper reset of DMA and ADC

--- ADCContinious with DMA/src/main.c
+++ ADCContinious with DMA/src/main.c
@@ -18,7 +18,7 @@
 #define Ain0 GPIO_Pin_0
 #define Ain0Channel ADC_Channel_0
 #define ADCPort GPIOA
-#define ADCBufferSize 10
+#define ADCBufferSize 1024
 
 GPIO_InitTypeDef GPIOPortA;  			// Struct for ADC1 GPIOA Config
 ADC_InitTypeDef ADCInit;				// Struct for ADC Config
@@ -26,7 +26,7 @@ ADC_CommonInitTypeDef ADCSetup;			// Struct for ADC prescaler clock value
 DMA_InitTypeDef DMASetup;				// Struct for DMA config
 
 
-static const uint32_t ADCDataBuffer[ADCBufferSize];		// Array to store data from ADC
+static uint32_t ADCDataBuffer[ADCBufferSize];		// Array to store data from ADC
 
 
 void initDMA(void)
@@ -39,14 +39,14 @@ void initDMA(void)
   DMASetup.DMA_PeripheralBaseAddr = (uint32_t)&ADC1->DR;				// Read from which register
   DMASetup.DMA_MemoryBaseAddr = (uint32_t)&ADCDataBuffer;  // Write to which address
   DMASetup.DMA_DIR = DMA_DIR_PeripheralSRC;					// Read from peripheral write to memory
-  DMASetup.DMA_BufferSize = 2;					// How many transfers from register to memory
+  DMASetup.DMA_BufferSize = ADCBufferSize;					// How many transfers from register to memory
   DMASetup.DMA_PeripheralInc = DMA_PeripheralInc_Disable;	// Don`t increment the peripheral address
   /*Memory increment doesn't work - buffer[i] oveflow */
   DMASetup.DMA_MemoryInc = DMA_MemoryInc_Enable;			// Increment the memory address Buffer[i] -> Buffer[i+1]
 
   DMASetup.DMA_PeripheralDataSize = DMA_PeripheralDataSize_HalfWord;	// 16 bits
-  DMASetup.DMA_MemoryDataSize = DMA_MemoryDataSize_Word;			// 16 bits
-  DMASetup.DMA_Mode = DMA_Mode_Circular;						// When all convertions are done, start again
+  DMASetup.DMA_MemoryDataSize = DMA_MemoryDataSize_Word;			// 32 bits
+  DMASetup.DMA_Mode = DMA_Mode_Normal;						// When all conversions are done, start again
   DMASetup.DMA_Priority = DMA_Priority_High;				// High priority of the channel
   DMASetup.DMA_M2M = DMA_M2M_Disable;						// Disable memory to memory transfer
 
@@ -98,7 +98,6 @@ void Init(void)
 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC1, ENABLE); // Enable clock for ADC 1
 	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOA, ENABLE); // Enable clock for GPIOA
 
-
 // *********************** Configure DMA ***************************************
 	initDMA();
 // *********************** Configure USART2 *************************************
@@ -119,27 +118,52 @@ void Delay(uint32_t nTime)
 	while(TimingDelay != 0);
 }
 
-/*
-uint16_t readADC1(uint8_t channel)
+void StartADCMeasure(void)
 {
-	ADC_RegularChannelConfig(ADC1, channel , 1, ADC_SampleTime_24Cycles);  //Configure the channel (PA.0, to be read)
-	ADC_SoftwareStartConv(ADC1);											// Start conversion
-	while(ADC_GetFlagStatus(ADC1, ADC_FLAG_EOC) == SET);					// Wait until conversion is done
-	return ADC_GetConversionValue(ADC1);
+	ADC_SoftwareStartConv(ADC1);
 }
-*/
 
+void ClearADCDataBuffer (void)
+{
+	int index = 0;
+	for(index = 0; index<= ADCBufferSize; index++)
+	{
+		ADCDataBuffer[index] = 0;
+	}
+}
+
+void ResetADC1(void)
+{
+	ADC_Cmd(ADC1, DISABLE);						// Disable the ADC1
+	ADC1->CR2 &= (uint32_t)(~ADC_CR2_SWSTART);
+	ADC_ContinuousModeCmd(ADC1, DISABLE);
+	while(ADC_GetFlagStatus(ADC1, ADC_FLAG_ADONS) == SET);  // Wait untill ADC1 is shutdown
+
+	ADC_Cmd(ADC1, ENABLE);						// Enable the ADC1
+	while(ADC_GetFlagStatus(ADC1, ADC_FLAG_ADONS) == RESET);  // Wait untill ADC1 is ON -> ADC Flag ADC on
+	ADC_ContinuousModeCmd(ADC1, ENABLE);
+}
+
+void ResetDMA(void)
+{
+	DMA_Cmd(DMA1_Channel1, DISABLE);
+	Delay(1);
+	DMA_SetCurrDataCounter(DMA1_Channel1, 1024);
+	DMA_Cmd(DMA1_Channel1, ENABLE);
+}
 int main(void)
 {
+	uint16_t DMATransfers;
 	Init();
-	uint16_t FlagStatusDMA;
-	uint16_t Ain;
 	ADC_RegularChannelConfig(ADC1, Ain0Channel , 1, ADC_SampleTime_24Cycles); //Configure the channel (PA.0, to be read)
 	while(1)
 	{
-		FlagStatusDMA = DMA_GetCurrDataCounter(DMA1_Channel1);
 		ADC_SoftwareStartConv(ADC1);
-		Ain = ADC_GetConversionValue(ADC1);			// Fetch data by software to compare against DMA ADC_DataBuffer
+		while(DMA_GetFlagStatus(DMA1_FLAG_TC1) != SET);
+		ResetADC1();
+		ResetDMA();
+		ClearADCDataBuffer();
+	//  ADC_Cmd(ADC1, ENABLE);
 		Delay(1000);
 	}
 
