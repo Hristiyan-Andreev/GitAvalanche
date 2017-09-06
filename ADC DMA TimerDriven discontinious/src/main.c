b7d89537942f4022d189d167bc67e1acc0d4c10d
Hristiyan Andreev <hrvalandreev@gmail.com>
2017-09-06 18:35:21 +0300

    ADC DMA Disctotinious Timer driven measurement added

--- ADC DMA TimerDriven discontinious/src/main.c
+++ ADC DMA TimerDriven discontinious/src/main.c
@@ -4,15 +4,23 @@
 #include "stm32l1xx_gpio.h"
 #include "stm32l1xx_dma.h"
 #include "stm32l1xx_tim.h"
-			
+
+//********************************** Define user constants ************************************
 #define Ain1 GPIO_Pin_1
 #define Ain1Channel ADC_Channel_1
 #define ADCPort GPIOA
-#define ADCBufferSize 10700
+
+#define ADCSampleTime 1					// In us
+#define ADCMeasureTime 10000			// In us
+#define ADCBufferSize 10000				// ADCMeasureTime / ADCSampleTime
 
 #define TriggerMeasurePin GPIO_Pin_10
 #define TriggerMeasurePort GPIOA
 
+uint32_t GlobalInterruptsDisabled;		// 0 if they are enabled, 1 if they are disabled
+
+uint32_t MeasureCycles = 0;
+//********************************* Define structures for initiliazation **********************
 GPIO_InitTypeDef GPIOPortA;  			// Struct for ADC1 GPIOA Config
 ADC_InitTypeDef ADCInit;				// Struct for ADC Config
 ADC_CommonInitTypeDef ADCSetup;			// Struct for ADC prescaler clock value
@@ -22,12 +30,12 @@ DMA_InitTypeDef DMASetup;				// Struct for DMA config
 TIM_TimeBaseInitTypeDef TimeBaseSetup; 	// Struct for TimeBase setup
 TIM_ICInitTypeDef InputCaptureSetup;	// Struct for input capture setup
 
+NVIC_InitTypeDef NVICSetup;				// Struct for NVIC config
+
 typedef enum STATE {ReadyToMeasure, MeasureStarted, MeasureEnded} STATE;
 STATE State;
 
-NVIC_InitTypeDef NVICSetup;				// Struct for NVIC config
-
-static uint32_t ADCDataBuffer[ADCBufferSize];		// Array to store data from ADC
+static __IO uint32_t ADCDataBuffer[ADCBufferSize];		// Array to store data from ADC
 
 
 void initDMA(void)
@@ -69,10 +77,10 @@ void initADC1(void)
 
 	ADCInit.ADC_Resolution = ADC_Resolution_12b; // Select resolution
 	ADCInit.ADC_ScanConvMode = DISABLE;			// Disable scan mode -> Measure only one input
-	ADCInit.ADC_ContinuousConvMode = ENABLE;   // Enable continious mode -> measure many times same channel
+	ADCInit.ADC_ContinuousConvMode = DISABLE;   // Disable cont mode -> Measure only on trigger
 	ADCInit.ADC_DataAlign = ADC_DataAlign_Right; // Align the 10bit data to the right
-	ADCInit.ADC_ExternalTrigConvEdge = ADC_ExternalTrigConvEdge_None; // Don't wait for edge to convert
-	ADCInit.ADC_ExternalTrigConv = ADC_ExternalTrigConv_T2_TRGO; // Trigger from Timer2
+	ADCInit.ADC_ExternalTrigConvEdge = ADC_ExternalTrigConvEdge_Rising; // Don't wait for edge to convert
+	ADCInit.ADC_ExternalTrigConv = ADC_ExternalTrigConv_T3_TRGO; // Trigger from Timer3
 	ADC_Init(ADC1, &ADCInit);					// Initialize the ADC Init struct for ADC1
 
 	ADCSetup.ADC_Prescaler = ADC_Prescaler_Div1; //Divide the HSI by 1 -> 16 Mhz (1Mhz conversion freq)
@@ -86,7 +94,7 @@ void initADC1(void)
 	ADC_Cmd(ADC1, ENABLE);						// Enable the ADC1
 
 
-	while(ADC_GetFlagStatus(ADC1, ADC_FLAG_ADONS) == RESET);  // Wait untill ADC1 is ON -> ADC Flag ADC on
+	while(ADC_GetFlagStatus(ADC1, ADC_FLAG_ADONS) == RESET);  // Wait until ADC1 is ON -> ADC Flag ADC on
 }
 
 void initTriggerMeasure(void)
@@ -100,45 +108,54 @@ void initTriggerMeasure(void)
 	GPIO_Init(TriggerMeasurePort, &GPIOPortA);			// Initialize Port A with the settings saved in the structure variable
 
 }
-
-void initTimers(void)
+void initTimers2(void)
 {
-// *************************** Set up Parameters ***************************************
 // *************************** Set up timer triggering *********************************
-	TIM_UpdateRequestConfig(TIM3, TIM_UpdateSource_Regular); // Only underflow/overflow can generate update interrupt
-	TIM_SelectOutputTrigger(TIM3, TIM_TRGOSource_Update); // TRGO event only from update event
+	TIM_UpdateRequestConfig(TIM2, TIM_UpdateSource_Regular); // Only underflow/overflow can generate update event
+	TIM_SelectOutputTrigger(TIM2, TIM_TRGOSource_Enable); // TRGO event only from enable event
 
-	TIM_SelectOnePulseMode(TIM3, TIM_OPMode_Single);	// Select OnePulse mode ->(Counter stops counting at the next update event
+	TIM_SelectOnePulseMode(TIM2, TIM_OPMode_Single);	// Select OnePulse mode ->Counter stops counting at the next update event
 
-	TIM_ETRConfig(TIM3, TIM_ExtTRGPSC_OFF, TIM_ExtTRGPolarity_NonInverted, 0); // Timer2, no division, positive polarity, no filtering
-	TIM_SelectInputTrigger(TIM3, TIM_TS_TI1F_ED); // Timer2, select TI1 source as TRGI
-	TIM_SelectSlaveMode(TIM3, TIM_SlaveMode_Trigger); // Timer2, configure in trigger mode (Start counting when TRGI is rising edge)
+	TIM_ETRConfig(TIM2, TIM_ExtTRGPSC_OFF, TIM_ExtTRGPolarity_NonInverted, 2); // Timer2, no division, positive polarity, filtering
+	TIM_SelectInputTrigger(TIM2, TIM_TS_ETRF);			// Select External trigger PA.0 to start the timmer from trigger measure
+	TIM_SelectSlaveMode(TIM2, TIM_SlaveMode_Trigger);  // Timer2, configure in trigger mode (Start counting when TRGI is rising edge)
 
-// *************************** Set up time base (ticks) ********************************
-	TimeBaseSetup.TIM_Prescaler = 32;					// Divide system core freq 32 times 32Mhz -> 1Mhz
+// *************************** Set up time base of TIM2 (stopping TIM3) (ticks) ********************************
+	TimeBaseSetup.TIM_Prescaler = 64;					// Divide system core freq 32 times 32Mhz -> 1Mhz
 	TimeBaseSetup.TIM_ClockDivision = TIM_CKD_DIV1;		// No further division of the freq
-	TimeBaseSetup.TIM_CounterMode = TIM_CounterMode_Down; // Counting up
-	TimeBaseSetup.TIM_Period = 10000;		// 10 000 us = 10ms measuring time
-	TIM_TimeBaseInit(TIM3, &TimeBaseSetup);
+	TimeBaseSetup.TIM_CounterMode = TIM_CounterMode_Down; // Counting down
+	TimeBaseSetup.TIM_Period = ADCMeasureTime;			// 10000us
+	TIM_TimeBaseInit(TIM2, &TimeBaseSetup);
 
-// *************************** Set up input capture *************************************
-	InputCaptureSetup.TIM_Channel = TIM_Channel_1;
-	InputCaptureSetup.TIM_ICPrescaler = TIM_CKD_DIV1;
-	InputCaptureSetup.TIM_ICFilter = 2;
-	InputCaptureSetup.TIM_ICPolarity = TIM_ICPolarity_Rising;
-	InputCaptureSetup.TIM_ICSelection = TIM_ICSelection_DirectTI;
-	TIM_ICInit(TIM3, &InputCaptureSetup);
-
-
-
-// Configure PA.6 for Timer2 Trigger
-	GPIOPortA.GPIO_Pin = GPIO_Pin_6;
+// *************************** Configure PA.6 for Timer3 Trigger ************************************************
+	GPIOPortA.GPIO_Pin = GPIO_Pin_0;
 	GPIOPortA.GPIO_Mode = GPIO_Mode_AF;
 	GPIOPortA.GPIO_Speed = GPIO_Speed_2MHz;
 	GPIOPortA.GPIO_OType = GPIO_OType_PP;
 	GPIOPortA.GPIO_PuPd = GPIO_PuPd_NOPULL;
 	GPIO_Init(GPIOA, &GPIOPortA);
-	GPIO_PinAFConfig(GPIOA, GPIO_PinSource6, GPIO_AF_TIM3);
+	GPIO_PinAFConfig(GPIOA, GPIO_PinSource0, GPIO_AF_TIM2);
+
+// Configure interrupt on reaching target cycles (10ms)
+	TIM_ITConfig(TIM2, TIM_IT_Update, ENABLE);
+}
+
+void initTimers3(void)
+{
+// *************************** Set up Parameters ***************************************
+// *************************** Set up timer triggering *********************************
+	TIM_UpdateRequestConfig(TIM3, TIM_UpdateSource_Regular); // Only underflow/overflow can generate update interrupt
+	TIM_SelectOutputTrigger(TIM3, TIM_TRGOSource_Update); // TRGO event only from counter enable event (TRGI event)
+
+	TIM_SelectInputTrigger(TIM3, TIM_TS_ITR1); // Timer3, select Internal trigger (TIM2) as trigger (page 423 user manual)
+	TIM_SelectSlaveMode(TIM3, TIM_SlaveMode_Trigger); // Timer2, configure in trigger mode (Start counting when TRGI is rising edge)
+
+// *************************** Set up time base of TIM3 (control of ADC) (ticks) ********************************
+	TimeBaseSetup.TIM_Prescaler = 32;					// Divide system core freq 32 times 32Mhz -> 1Mhz
+	TimeBaseSetup.TIM_ClockDivision = TIM_CKD_DIV1;		// No further division of the freq
+	TimeBaseSetup.TIM_CounterMode = TIM_CounterMode_Down; // Counting down
+	TimeBaseSetup.TIM_Period = ADCSampleTime;		// 1us
+	TIM_TimeBaseInit(TIM3, &TimeBaseSetup);
 
 // Configure interrupt on reaching target cycles (10ms)
 	TIM_ITConfig(TIM3, TIM_IT_Update, ENABLE);
@@ -148,11 +165,18 @@ void initNVIC(void)
 {
 // ****************** Setup the Timer count complete interrupt *****************************
 	NVICSetup.NVIC_IRQChannel = TIM3_IRQn; 		// Enable USART2 Interrupts
-	NVICSetup.NVIC_IRQChannelPreemptionPriority = 5;
-	NVICSetup.NVIC_IRQChannelSubPriority = 5;
+	NVICSetup.NVIC_IRQChannelPreemptionPriority = 2;
+	NVICSetup.NVIC_IRQChannelSubPriority = 0;
+	NVICSetup.NVIC_IRQChannelCmd = ENABLE;			// Enable global interrupts
+	NVIC_Init(&NVICSetup);
+
+	NVICSetup.NVIC_IRQChannel = TIM2_IRQn; 		// Enable USART2 Interrupts
+	NVICSetup.NVIC_IRQChannelPreemptionPriority = 3;
+	NVICSetup.NVIC_IRQChannelSubPriority = 0;
 	NVICSetup.NVIC_IRQChannelCmd = ENABLE;			// Enable global interrupts
 	NVIC_Init(&NVICSetup);
 }
+
 
 void Init(void)
 {
@@ -161,13 +185,11 @@ void Init(void)
 	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_DMA1, ENABLE);    // Enable clock for DMA1
 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC1, ENABLE); // Enable clock for ADC 1
 	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOA, ENABLE); // Enable clock for GPIOA
-	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, ENABLE); // Enable clock for Timer9
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE); // Enable clock for Timer2
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, ENABLE); // Enable clock for Timer3
 
-	if (SysTick_Config(SystemCoreClock / 1000))
-	{
-		while(1);
-	}
-
+// ********************** Configure SysTick Timer ******************************
+	if (SysTick_Config(SystemCoreClock / 1000))	{ while(1);	}
 // ********************** Configure interrupt controller ****************************
 	initNVIC();
 // *********************** Configure DMA ***************************************
@@ -176,14 +198,9 @@ void Init(void)
 	initADC1();
 // ********************** Configure Trigger for start of measure******************************
 	initTriggerMeasure();
-
 // ********************** Configure timer system *************************************
-	initTimers();
-
-
-
-// ********************** Configure SysTick Timer ******************************
-
+	initTimers2();
+	initTimers3();
 }
 
 static __IO uint32_t TimingDelay;   // Decrementing value for delay
@@ -192,6 +209,22 @@ void Delay_ms(uint32_t nTime)
 {
 	TimingDelay = nTime;
 	while(TimingDelay != 0);
+}
+
+void TriggerMeasure()
+{
+	GPIO_SetBits(TriggerMeasurePort, TriggerMeasurePin);
+	State = MeasureStarted;
+	Delay_ms(1);
+	GPIO_ResetBits(TriggerMeasurePort, TriggerMeasurePin);
+}
+
+void ResetDMA(void)
+{
+	DMA_Cmd(DMA1_Channel1, DISABLE);
+	Delay_ms(1);
+	DMA_SetCurrDataCounter(DMA1_Channel1, ADCBufferSize);
+	DMA_Cmd(DMA1_Channel1, ENABLE);
 }
 
 void ClearADCDataBuffer (void)
@@ -203,44 +236,15 @@ void ClearADCDataBuffer (void)
 	}
 }
 
-void StopADCMeasure(void)
-{
-	ADC_Cmd(ADC1, DISABLE);						// Disable the ADC1
-	ADC1->CR2 &= (uint32_t)(~ADC_CR2_SWSTART);
-	State = MeasureEnded;
-}
-
-void ResetADC1(void)
-{
-	ADC_ContinuousModeCmd(ADC1, DISABLE);
-	while(ADC_GetFlagStatus(ADC1, ADC_FLAG_ADONS) == SET);  // Wait untill ADC1 is shutdown
-
-	ADC_Cmd(ADC1, ENABLE);						// Enable the ADC1
-	while(ADC_GetFlagStatus(ADC1, ADC_FLAG_ADONS) == RESET);  // Wait untill ADC1 is ON -> ADC Flag ADC on
-	ADC_ContinuousModeCmd(ADC1, ENABLE);
-}
-
-void ResetDMA(void)
-{
-	DMA_Cmd(DMA1_Channel1, DISABLE);
-	Delay_ms(1);
-	DMA_SetCurrDataCounter(DMA1_Channel1, ADCBufferSize);
-	DMA_Cmd(DMA1_Channel1, ENABLE);
-}
-void TriggerMeasure()
-{
-	GPIO_SetBits(TriggerMeasurePort, TriggerMeasurePin);
-	ADC_SoftwareStartConv(ADC1);
-	State = MeasureStarted;
-	Delay_ms(1);
-	GPIO_ResetBits(TriggerMeasurePort, TriggerMeasurePin);
-}
+void CheckAndDisableInterrupts(void) { GlobalInterruptsDisabled = __get_PRIMASK();__disable_irq(); }
+void CheckAndEnableInterrupts(void) { if(!GlobalInterruptsDisabled) __enable_irq(); }
 int main(void)
 {
+	CheckAndDisableInterrupts();
 	uint32_t readingCycles = 0;
-	uint8_t RCC_Source = RCC_GetSYSCLKSource();
 	Init();
 	ADC_RegularChannelConfig(ADC1, Ain1Channel , 1, ADC_SampleTime_4Cycles); //Configure the channel (PA.1, to be read)
+	CheckAndEnableInterrupts();
 	while(1)
 	{
 		State = ReadyToMeasure;
@@ -248,10 +252,10 @@ int main(void)
 		Delay_ms(50);
 		if (State == MeasureEnded)
 		{
-			ResetADC1();
 			ResetDMA();
 			ClearADCDataBuffer();
 			readingCycles++;
+			MeasureCycles = 0;
 		}
 
 		Delay_ms(1000);
@@ -268,14 +272,21 @@ void SysTick_Handler(void)
 	}
 }
 
-
 void TIM3_IRQHandler (void)
 {
 	if(TIM_GetITStatus(TIM3, TIM_IT_Update) != RESET)  // If counter update event had occurred
 	{
-		StopADCMeasure();
-		TIM_ClearFlag(TIM3, TIM_FLAG_Update);			// Clear the TIM3 Update event flag
+		MeasureCycles++;
 		TIM_ClearITPendingBit(TIM3,TIM_IT_Update);		// Clear the TIM3 Update event IT flag -> same as top
 	}
 }
 
+void TIM2_IRQHandler (void)
+{
+	if(TIM_GetITStatus(TIM2, TIM_IT_Update) != RESET)  // If counter update event had occurred
+	{
+		TIM3->CR1 &= (~TIM_CR1_CEN);
+		TIM_ClearITPendingBit(TIM2,TIM_IT_Update);		// Clear the TIM3 Update event IT flag -> same as top
+		State = MeasureEnded;
+	}
+}
